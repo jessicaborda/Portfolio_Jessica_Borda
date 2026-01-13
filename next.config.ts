518120edc1ffabbdb70e9f9084f892a325f91337
--get <--get>
2026-01-12 22:14:52 -0500

    Deploy-GitHub actions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/Portfolio_Jessica_Borda",
 };
 
 export default nextConfig;
